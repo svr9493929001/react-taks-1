3bf4420a36bc2e1d614fbfdb0183ef646f9482fc
svr9493929001 <svr9493929001@gmail.com>
2021-11-29 21:41:34 +0530

    react-task-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Data = [
+  {
+    title: "FREE",
+    price: "0",
+    users: "✔️ Single User",
+    storage: "✔️ 5GB Storage",
+    Public: "✔️ Unlimited Public Projects",
+    access: "✔️ Community Access",
+    Private: "❌ Unliited Private Projects",
+    Support: "❌ Dedicated Phone Support",
+    Subdmain: "❌ Free Subdomain",
+    Monthly: "❌ Monthly Status Reports",
+  },
+  {
+    title: "PLUS",
+    price: "9",
+    users: "✔️ 5 User",
+    storage: "✔️ 50GB Storage",
+    Public: "✔️ Unlimited Public Projects",
+    access: "✔️ Community Access",
+    Private: "✔️ Unliited Private Projects",
+    Support: "✔️ Dedicated Phone Support",
+    Subdmain: "✔️ Free Subdomain",
+    Monthly: "❌ Monthly Status Reports",
+  },
+  {
+    title: "PRO",
+    price: "49",
+    users: "✔️ 5 User",
+    storage: "✔️ 150GB Storage",
+    Public: "✔️ Unlimited Public Projects",
+    access: "✔️ Community Access",
+    Private: "✔️ Unliited Private Projects",
+    Support: "✔️ Dedicated Phone Support",
+    Subdmain: "✔️ Unlimited Free Subdomain",
+    Monthly: "✔️ Monthly Status Reports",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Data.map((ele, i) => (
+        <Card
+          key={i}
+          title={ele.title}
+          price={ele.price}
+          users={ele.users}
+          storage={ele.storage}
+          Public={ele.Public}
+          access={ele.access}
+          Private={ele.Private}
+          Support={ele.Support}
+          Subdmain={ele.Subdmain}
+          Monthly={ele.Monthly}
+          id ={i}
+        />
+      ))}
     </div>
   );
+  function Card({title,price,users,storage,Public,access,Private,Support,Subdmain,Monthly,id}) {
+    let styles
+    {id === 0 ?  (styles = { opacity: "50%" }) : (styles = { opacity: "100%" })}
+    let s 
+    {id === 1 || id === 0 ?  (s = { opacity: "50%" }) : (s = { opacity: "100%" })}
+    return (
+      <div className="container">
+        <h3>{title}</h3>
+        <h1>
+          ${price} <span>/month</span>{" "}
+        </h1>
+        <hr></hr>
+        <div className="offers">
+          <h3>{users}</h3>
+          <h3>{storage}</h3>
+          <h3>{Public}</h3>
+          <h3>{access}</h3>
+          <h3>{Private}</h3>
+          <h3 style={styles}>{Support}</h3>
+          <h3 style={styles}>{Subdmain}</h3>
+          <h3 style={s}>{Monthly}</h3>
+        </div>
+        <button>BUTTON</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
